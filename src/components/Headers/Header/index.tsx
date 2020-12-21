7d5a7a8f205bf4d4f9e4bbbd67d33e71131a4c70
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2020-12-21 17:07:46 -0300

    feat: added submenu on Header

--- src/components/Headers/Header/index.tsx
+++ src/components/Headers/Header/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
+  const [productsMenu, setProductsMenu] = useState({
+    active: false,
+  });
+
   return (
     <header className={styles.header}>
       <div className="container">
         <div className="row">
           <div className="col-md-auto">
-            <img src="./images/logo.png" alt="PolySteel" />
+            <a href="./">
+              <img src="./images/logo.png" alt="PolySteel" />
+            </a>
           </div>
           <div className={`${styles.searchInput} col-md`}>
             <div>
@@ -22,10 +28,106 @@ const Header: React.FC = () => {
             </div>
           </div>
           <div className={`${styles.iconsMenu} col-md-3`}>
-            <a href="/products" className={styles.productBtn}>
+            <button
+              type="button"
+              className={styles.productBtn}
+              onClick={() => {
+                setProductsMenu({ active: !productsMenu.active });
+              }}
+            >
               Produtos
               <i className="fa fa-bars" />
-            </a>
+            </button>
+            <ul
+              className={
+                productsMenu.active === true ? styles.submenuActive : ''
+              }
+            >
+              <li>
+                <span>PLACAS</span>
+              </li>
+              <li>
+                <a href="#a">Placa de Premiação</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Agradecimento</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Parabenizar</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Parabenização</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Tempo de Casa</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Reconhecimento</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Inauguração</a>
+              </li>
+              <li>
+                <a href="#a">Plaquetas</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Identiﬁcação (Interna)</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Identiﬁcação (Externa)</a>
+              </li>
+              <li>
+                <a href="#a">Placa de Sinalização</a>
+              </li>
+              <li>
+                <a href="#a">Placas de Acrílico (Diversos)</a>
+              </li>
+              <li>
+                <a href="#a">Placas de Aço Inox (Diversos)</a>
+              </li>
+              <li>
+                <a href="#a">Placas Diversas</a>
+              </li>
+              <li>
+                <span>Outros</span>
+              </li>
+              <li>
+                <a href="#a">Troféus</a>
+              </li>
+              <li>
+                <a href="#a">Totem</a>
+              </li>
+              <li>
+                <a href="#a">Letra Caixa</a>
+              </li>
+              <li>
+                <a href="#a">Banners</a>
+              </li>
+              <li>
+                <a href="#a">Túmulo/Jazigo</a>
+              </li>
+              <li>
+                <a href="#a">Crachá</a>
+              </li>
+              <li>
+                <a href="#a">Diploma</a>
+              </li>
+              <li>
+                <span>ADESIVOS</span>
+              </li>
+              <li>
+                <a href="#a">Adesivos 1m² entregue sem corte</a>
+              </li>
+              <li>
+                <a href="#a">Adesivo 1m² com corte eletrônico</a>
+              </li>
+              <li className={styles.allProducts}>
+                <a href="/products/">
+                  Todos os Produtos
+                  <i className="fas fa-arrow-right" />
+                </a>
+              </li>
+            </ul>
             <a href="/user">
               <i className="fa fa-user" />
             </a>
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Head, Header, Footer } from '../../components';
+
+const Products = (): JSX.Element => {
+  return (
+    <>
+      <Head
+        title="Produtos - PolySteel"
+        description="A Polysteel oferece diversas opções de serviços gráficos como Placas de Homenagem, Placas de Aço Inox, Letras Caixa e mais."
+        keywords="placa de homenagem, placa de aço, troféus, banners, letras caixa, gráfica, gráfica rápida, placas adesivadas, placas com molduras, gráfica em são paulo, melhor serviço de gráfica em são paulo, melhor gráfica, gráfica mais rápida, melhor preço em placa em aço inox"
+      />
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Products;
