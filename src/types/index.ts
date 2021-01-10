9a294a562656915d0fe31101d2c1df408caf0b88
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-01-10 10:35:48 -0300

    fix: header type

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,7 +6,4 @@ export * from './Breadcrumb';
 export * from './User';
 export * from './Order';
 export * from './Context';
-<<<<<<< HEAD
 export * from './Button';
-=======
->>>>>>> b92afcadb1305a227a7a60b5d98d1489916ae625
